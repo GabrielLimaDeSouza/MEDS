97c63e43d90c1a99159af8e3b3b9862df6e4a1a8
Gabriel Lima de Souza <90854484+GabrielLimaDeSouza@users.noreply.github.com>
2021-12-03 19:52:16 -0300

    Update and rename script.js to agenda.js

--- Codigo/js/agenda.js
+++ Codigo/js/agenda.js
@@ -1,6 +1,37 @@
 document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar');
 
+
+  var db_horarios = {
+    "data": [
+        {
+            "nome": "João Marcelo da Silva",
+            "data": "2021-12-01",
+            "horario": "08:00",
+            "inicio": "2021-12-01T08:00"
+        },
+        {
+            "nome": "Isabel Lopes dos Reis",
+            "data": "2021-11-01",
+            "horario": "15:00",
+            "inicio": "2021-11-01T15:00"
+        },
+        {
+            "nome": "Julia de Souza Mendes",
+            "data": "2021-10-15",
+            "horario": "17:00",
+            "inicio": "2021-10-15T17:00"
+        }
+    ]
+  }
+
+  let db = JSON.parse(localStorage.getItem("horario"));
+  if (!db)
+  {
+    db = db_horarios;
+    localStorage.setItem("horario",JSON.stringify(db));
+  }
+
   var calendar = new FullCalendar.Calendar(calendarEl, {
     locale: 'pt-br',
     initialView: 'timeGridDay',
@@ -15,37 +46,27 @@ document.addEventListener('DOMContentLoaded', function() {
         text: 'Clique para cadastrar sua agenda médica',
         click: function() {
           var dateStr = prompt('Coloque uma data no formato <ANO-MES-DIA>');
-          var horario = prompt('Digite o horário em formato <XX:ZZ>');
+          var horas = prompt('Digite o horário em formato <XX:ZZ>');
           var date = new Date(dateStr + 'T00:00:00'); 
 
           if (!isNaN(date.valueOf())) { 
             calendar.addEvent({
               title: 'Horario disponível',
-              start: dateStr+"T"+horario
+              date: dateStr,
+              start: dateStr+"T"+horas
             });
             
-            var novoHorario = [
-              {
-                medico: document.getElementById("medico").value,
-                data: dateStr,
-                horario: [
-                  {
-                    horario
-                  }
-                ],
-                inicio: dateStr+"T"+horario
-              }
-            ];
+            var novoHorario = {
+              medico: document.getElementById("medico").value,
+              data: dateStr,
+              horario: horas,
+              inicio: dateStr+"T"+horas
+            };
 
             var db = JSON.parse(localStorage.getItem("horario"));
-            if (!db)
+            if (db)
             {
-              db = novoHorario;
-              localStorage.setItem("horario",JSON.stringify(db));
-            }
-            else
-            {
-              db.push(novoHorario);
+              db.data.push(novoHorario);
               localStorage.setItem("horario",JSON.stringify(db));
             }
 
