3c0239525e10b63a2ab12503ecda294de5429ffc
Gabriel Lima de Souza <gabriel.souza.1354648@sga.pucminas.br>
2021-12-13 16:17:49 -0300

    Adição do rodapé em todas as páginas e alguns botoões

--- Codigo/solicitar_laudo/laudo.js
+++ Codigo/solicitar_laudo/laudo.js
@@ -11,3 +11,36 @@ usuario.forEach((nome) => {
     select.options[select.options.length] = option;
 });
 
+var solicitacao_laudo_inicial = {
+    "data": [
+        {
+            "nome": "Julia Lima",
+            "mensagem": "Preciso de um laudo médico para fazer um exame trabalhista",
+            "medico": "Gabriel Lima de Souza"
+        },
+    ]
+}
+
+var db = JSON.parse(localStorage.getItem('solicitacaoLaudo'));
+if (!db) {
+    db = solicitacao_laudo_inicial;
+    localStorage.setItem('solicitacaoLaudo', JSON.stringify(db));
+};
+
+var btn = document.getElementById("btnsubmit");
+btn.addEventListener("click",function(){
+    let mensagem = document.getElementById("inputmensagem").value;
+    let medico = document.getElementById("selectmedico").value;
+    var usermoment = JSON.parse(sessionStorage.getItem('usuarioCorrenteP'));
+    let solicitante = usermoment.nome;
+
+    let novaSolicitacao = {
+        "nome": solicitante,
+        "mensagem": mensagem,
+        "medico": medico
+    };
+
+    db.data.push(novaSolicitacao);
+    localStorage.setItem('solicitacaoLaudo', JSON.stringify(db));
+
+});
